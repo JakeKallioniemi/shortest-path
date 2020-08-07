05806998a2dfa0ec8f6427ac209c42b37327fbbf
Jake Kallioniemi <37346883+JakeKallioniemi@users.noreply.github.com>
2020-08-07 23:10:16 +0300

    Create interface for math and string util classes

---
+++ src/main/java/shortestpath/util/MathUtil.java
@@ -0,0 +1,39 @@
+package shortestpath.util;
+
+/**
+ * Constants and math functions.
+ * 
+ * @author Jake
+ */
+public class MathUtil {
+
+    /**
+     * Approximate value of square root of 2.
+     */
+    public static double SQRT_OF_TWO = 1.4142135623730950488016887242097;
+    
+    /**
+     * Rounds a number to given amount of decimal places.
+     * 
+     * @param value to be rounded
+     * @param decimalPlaces amount of decimal places
+     * @return rounded value
+     */
+    public static double round(double value, int decimalPlaces) {
+        // TODO
+        throw new UnsupportedOperationException();
+    }
+    
+    /**
+     * Checks if two numbers are equal to given amount of decimal places.
+     * 
+     * @param a first number
+     * @param b seconds number
+     * @param decimalPlaces amount of decimal places
+     * @return true if equal to given amount of decimal places otherwise false
+     */
+    public static boolean equals(double a, double b, int decimalPlaces) {
+        // TODO
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ src/main/java/shortestpath/util/StringUtil.java
@@ -0,0 +1,44 @@
+package shortestpath.util;
+
+/**
+ * Various utility functions for Strings.
+ * 
+ * @author Jake
+ */
+public class StringUtil {
+
+    /**
+     * Gets a substring of the given String.
+     * 
+     * @param string a String
+     * @param from start index inclusive
+     * @param to end index exclusive
+     * @return substring
+     */
+    public static String substring(String string, int from, int to) {
+        // TODO
+        throw new UnsupportedOperationException();
+    }
+    
+    /**
+     * Check if the String represents and integer.
+     * 
+     * @param string a String
+     * @return true if String represents an integer otherwise false
+     */
+    public static boolean isInteger(String string) {
+        // TODO
+        throw new UnsupportedOperationException();
+    }
+    
+    /**
+     * Attempts to parse a String to int.
+     * 
+     * @param string integer String
+     * @return int parsed from the input String
+     */
+    public static int toInt(String string) {
+        // TODO
+        throw new UnsupportedOperationException();
+    }
+}
